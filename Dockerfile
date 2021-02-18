530bf034c76859c6700d20b4dbd0444d7e8e74ba
whitleyo <owen.whitley@mail.utoronto.ca>
2021-02-18 13:07:21 -0500

    got Dockerfile + setup to work

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM ubuntu:20.04
 RUN apt-get update
 RUN apt-get install -y wget
+RUN apt-get install -y vim
+RUN apt-get install -y less
+RUN touch /root/.bashrc
 RUN mkdir /src
 COPY src/setup.sh /src/
 RUN chmod +x /src/setup.sh
 RUN /src/setup.sh
+RUN echo 'export PATH="$PATH:/root/sratoolkit.2.10.9-ubuntu64/bin"' >> /root/.bashrc
+
--- src/setup.sh
+++ src/setup.sh
@@ -4,11 +4,16 @@ set -e
 #apt-get install wget
 # install anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
-bash Anaconda3-2020.11-Linux-x86_64.sh -y
+bash Anaconda3-2020.11-Linux-x86_64.sh -b -p /root/anaconda
+eval "$(/root/anaconda/bin/conda shell.bash hook)"
+conda init
 # create microbiome environment
 conda create -n microbiome python=3.7 -y
 conda activate microbiome
 # installs metaphlan for getting bacterial species frequencies 
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
 conda install  -y -c bioconda python=3.7 metaphlan
 # other useful packages
 conda install numpy -y
@@ -18,3 +23,7 @@ conda install seaborn -y
 conda install scikit-learn -y
 conda install jupyter -y
 conda deactivate
+
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.10.9/sratoolkit.2.10.9-ubuntu64.tar.gz
+tar -xvzf sratoolkit.2.10.9-ubuntu64.tar.gz
+mv sratoolkit.2.10.9-ubuntu64 /root/
