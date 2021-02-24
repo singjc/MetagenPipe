ba086b26e7b10576d4d85c7ac44f4adfc884d148
Justin Sing <justincsing@gmail.com>
2021-02-24 02:09:07 -0500

    [FEATURE] Added class frequency and confusion matrix plts

--- run_docker_it.sh
+++ run_docker_it.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#############
+## CL Args
+#############
+working_dir=${1:-`pwd`}
 # interactively run docker. Note that we have the v option for binding our project directory to a /project directory
 # un docker image. Downloaded or modified data within docker image that resides within /project directory will persist
 # on host OS location, and will be visible on host OS
-docker run -v /home/owenwhitley/projects/microbiome:/project -p 8888:8888 -it microbiome_ubuntu20 
+docker run -v $working_dir:/project -p 8888:8888 -it whitleyo/microbiome_ubuntu20:latest
