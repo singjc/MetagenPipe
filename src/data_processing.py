17b86a0db6e8705feabe9aab59148dc053e863da
whitleyo <owen.whitley@mail.utoronto.ca>
2021-05-09 22:57:40 -0400

    [FEATURE] added functions to parse metaphlan output. TODO: do dry run on metaphlan output files

--- src/data_processing.py
+++ src/data_processing.py
@@ -46,12 +46,14 @@ def seqtk_call( fastq_file, subsample_fraction, two_pass_mode=False, rng_seed=10
             click.echo( f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] INFO: Subsampled file written to {fastq_subsampled_file}" )
             break
 
+
 def check_kneaddata_install():
     '''
-    Check to see if seqtk is installed
+    Check to see if kneaddata is installed
     '''
     exitcode = subprocess.getstatusoutput("kneaddata")[0]
     assert( exitcode==127 ), "Could not verify kneaddata! Make sure you have the program installed!"
+
 
 def kneaddata_call( fastq_file, reference_db, output_dir, **kwargs ):
     ## Check to make sure kneaddate is installed.
@@ -132,6 +134,12 @@ def run_kneaddata( fastq_files, reference_db, output_dir, nthreads ):
     pool.join()
 
 # MetaPhlan 3.0
+def check_metaphlan_install():
+    '''
+    Check to see if metaphlan is installed
+    '''
+    exitcode = subprocess.getstatusoutput("metaphlan")[0]
+    assert( exitcode==127 ), "Could not verify metaphlan! Make sure you have the program installed!"
 
 def metaphlan_call( input_file, input_type, output_dir_bowtie, output_dir_profile, nthreads ):
     """
@@ -193,6 +201,80 @@ def run_metaphlan( inp_files, input_type, output_dir_bowtie, output_dir_profile,
 
     for f in inp_files:
         metaphlan_call(f, input_type, output_dir_bowtie, output_dir_profile, nthreads)
+
+
+def parse_metaphlan_file( input_file ):
+    """
+    Parse metaphlan output file
+    :param input_file: str, filepath to input file
+    :return: 1 row pandas dataframe, with species abundances in columns
+
+    Note: expect that basename of file is unique
+
+    """
+    input = pd.read_csv(input_file, delimiter='\t', skiprows=4, header=False)
+    # taxa is in 1st col, relative abundance in 3rd. See https://github.com/biobakery/biobakery/wiki/metaphlan3#output-files
+    # for more details
+    taxa = input.iloc[: , 0].to_numpy().astype('str')
+    rel_abundance = input.iloc[:, 2].to_numpy().astype('float32')
+    species_idx = np.where(np.array([re.search('s__', x) is not None for x in taxa]))[0]
+    dict_out = {}
+    for i in species_idx:
+        taxa_name = taxa[i]
+        dict_out[taxa_name] = np.array([rel_abundance[i]]).astype('float32')
+    # get rid of file ending for row name for output dataframe
+    row_name = re.sub('\\.[A-z0-9]+', '', os.path.basename(input_file))
+    pd_df_out = pd.DataFrame(dict_out, index=)
+    try:
+        # check that relative abundance sums to 100 percent
+        total_pct = pd_df_out.to_numpy(dtype='float32').sum()
+        assert np.equal(total_pct, 100.0)
+    except:
+        raise ValueError('total relative abundance ' + total_pct + ' is not 100.0')
+
+    return pd_df_out
+
+
+@cli.command()
+@click.argument('inp_files', nargs=-1, type=click.Path(exists=True))
+@click.option('--output_dir', default=(os.getcwd()), show_default=True, type=str, help='Directory to store matrix output')
+@click.option('--outfile', default=('relative_abundances.csv'), show_default=True, type=str, help='filename for matrix output')
+def parse_metaphlan_multi( inp_files, output_dir='./', outfile='relative_abundances.csv' ):
+    """
+    Parse a list or iterable of metaphlan output files
+    :param inp_files: input files. list or iterable of strings
+    :param output_dir: output directory to store matrix
+    :return: writes matrix to file
+    """
+    for i in range(len(inp_files)):
+        input_file_i = inp_files[i]
+        df_i = parse_metaphlan_file(input_file_i)
+        if i == 0:
+            # df_c = dataframe, combined
+            df_c = df_i
+        else:
+            cols_c = df_c.columns.to_numpy().astype('str')
+            cols_i = df_i.columns.to_numpy().astype('str')
+            diff_c_i = np.setdiff1d(cols_c, cols_i)
+            diff_i_c = np.setdiff1d(cols_i, cols_c)
+            if len(diff_c_i) > 0:
+                for j in len(diff_c_i):
+                    diff_c_i_j = diff_c_i[j]
+                    df_i[diff_c_i_j] = np.array([0.0], dtype='float32')
+
+            if len(diff_i_c) > 0:
+                for j in len(diff_i_c):
+                    diff_i_c_j = diff_i_c[j]
+                    df_c[diff_i_c_j] = np.array([0.0], dtype='float32')
+            # put columns in correct order, then append.
+            # by end of 2 if statements, both df_c and df_i should have
+            # same columns
+            sorted_cols = df_c.columns.to_numpy().astype('str').sort()
+            df_c = df_c.loc[:, sorted_cols]
+            df_i = df_i.loc[:, sorted_cols]
+            df_c.append(df_i)
+
+    df_c.to_csv(os.path.join(output_dir, outfile))
 
 
 if __name__ == '__main__':
