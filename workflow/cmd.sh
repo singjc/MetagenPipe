1c69ca172dc02791470f55f815f6e7da97f2d1f1
whitleyo <owen.whitley@mail.utoronto.ca>
2021-06-06 13:59:57 -0400

    workflow/Snakefile.subsample_kraken2_wf: attempt fix by removing && from matrix step, still getting unexpected token newline error

--- workflow/cmd.sh
+++ workflow/cmd.sh
@@ -1,5 +1,6 @@
 #!/bin/usr/sh
-
+# set for failure on any error
+set -e
 # Activate Conda env
 source /root/anaconda/etc/profile.d/conda.sh
 conda activate microbiome
@@ -10,14 +11,18 @@ conda activate microbiome
 #snakemake --snakefile Snakefile.sradownload_subs_wf -j 1
 
 # Preprocessing Workflow
+
 #snakemake --snakefile Snakefile.subsample_wf -j 6
+# to unlock directory
+snakemake --snakefile Snakefile.subsample_kraken2_wf --unlock True
 
-snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6 --config master_output_dir='kraken2_5M' reads_subsample=5000000
-snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6 --config master_output_dir='kraken2_1M' reads_subsample=1000000
-nakemake --snakefile Snakefile.subsample_kraken2_wf -j 6 --config master_output_dir='kraken2_500K' reads_subsample=500000
-snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6 --config master_output_dir='kraken2_100K' reads_subsample=100000
-snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6 --config master_output_dir='kraken2_50K' reads_subsample=50000
-snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6 --config master_output_dir='kraken2_10K' reads_subsample=10000
-
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_10M' reads_subsample=10000000
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_5M' reads_subsample=5000000
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_1M' reads_subsample=1000000
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_500K' reads_subsample=500000
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_100K' reads_subsample=100000
+snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_50K' reads_subsample=50000
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_25K' reads_subsample=25000
+# snakemake --snakefile Snakefile.subsample_kraken2_wf -j 6  --config master_output_dir='kraken2_10K' reads_subsample=10000
 
 conda deactivate
