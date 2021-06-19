88b4b51de422f2a3b8c8cc13ae821c7a0c9992a6
singjc <justincsing@gmail.com>
2021-06-19 00:17:06 -0400

    [ADD] Experiments from subsampling experiments

---
+++ bin/run_papermill_compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+working_dir=${1:-`pwd`}
+# docker run -d microbiome_ubuntu20 /bin/bash  /project/workflow/cmd.sh
+docker run --rm -w /project/workflow -v $working_dir:/project whitleyo/microbiome_ubuntu20:latest /bin/bash /project/workflow/cmd_papermill_compile_wf.sh
