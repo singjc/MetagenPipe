6010aa7872d5c23dce03d71d77cd88b5545e40e2
whitleyo <owen.whitley@mail.utoronto.ca>
2022-01-25 12:48:14 -0500

    [MODIFY] rewrite of metaphlan parser to be simplified and more consistent with results of kraken parser. TODO: deduplicate parser loop code for separate parser functions

--- src/preprocessing/data_processing.py
+++ src/preprocessing/data_processing.py
@@ -230,29 +230,17 @@ def parse_metaphlan_file( input_file ):
     Note: expect that basename of file is unique
 
     """
-    input = pd.read_csv(input_file, delimiter='\t', skiprows=4, header=None)
+    df = pd.read_csv(input_file, delimiter='\t', skiprows=4, header=None).iloc[:, np.array([0, 2])]
     # taxa is in 1st col, relative abundance in 3rd. See https://github.com/biobakery/biobakery/wiki/metaphlan3#output-files
     # for more details
-    taxa = input.iloc[: , 0].to_numpy().astype('str')
-    rel_abundance = input.iloc[:, 2].to_numpy().astype('float32')
-    species_idx = np.where(np.array([re.search('s__', x) is not None for x in taxa]))[0]
-    dict_out = {}
-
-    if len(species_idx) > 0:
-        for i in species_idx:
-            taxa_name = taxa[i]
-            dict_out[taxa_name] = np.array([rel_abundance[i]]).astype('float32')
-    # get rid of file ending for row name for output dataframe
-    row_name = re.sub('\\.[A-z0-9]+', '', os.path.basename(input_file))
-    pd_df_out = pd.DataFrame(dict_out, index=row_name)
-    # try:
-    #     # check that relative abundance sums to 100 percent
-    #     total_pct = pd_df_out.to_numpy(dtype='float32').sum()
-    #     assert np.equal(total_pct, 100.0)
-    # except:
-    #     raise ValueError('total relative abundance ' + total_pct + ' is not 100.0')
-
-    return pd_df_out
+    df.columns = ['tax_name', 'freq']
+    # filter for only taxa with species
+    taxa = df['tax_name'].to_numpy().astype('str')
+    species_idx = np.argwhere(np.array([re.search('s__', x) is not None for x in taxa])).flatten()
+    # pandas gracefully handles cases where species_idx is of length 0 by
+    # returning empty dataframe with appropriate columns
+    df = df.iloc[species_idx, :]
+    return df
 
 def kraken2_call( fastq1, db_use, reads_file, freq_file, nthreads=1, fastq2=None ):
     """
